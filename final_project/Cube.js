868661ad8b8cd4540b961bd4c19376b93f35f7d9
EdgintonJ <90742881+EdgintonJ@users.noreply.github.com>
2022-05-19 17:35:39 -0700

    Update Cube.js

--- final_project/Cube.js
+++ final_project/Cube.js
@@ -2,82 +2,7 @@
 
 function Cube() {
 
-    /* // Initialize the shader pipeline for this object using either shader ids
-    //   declared in the application's HTML header, or use the default names.
-    //
-    var vertShdr = vertexShaderId || "Cube-vertex-shader";
-    var fragShdr = fragmentShaderId || "Cube-fragment-shader";
-
-    this.program = initShaders(gl, vertShdr, fragShdr);
-
-    if ( this.program < 0 ) {
-        alert( "Error: Cone shader pipeline failed to compile.\n\n" +
-            "\tvertex shader id:  \t" + vertShdr + "\n" +
-            "\tfragment shader id:\t" + fragShdr + "\n" );
-        return; 
-    }
-
-    var n = numSides || DefaultNumSides; // Number of sides
-
-    var theta = 0.0;
-    var dTheta = 2.0 * Math.PI / n;
-    
-    // Record the number of components for each vertex's position in the Cone object's 
-    //   positions property. (that's why there's a "this" preceding the positions here).
-    //   Here, we both create the positions object, as well as initialize its
-    //   numComponents field.
-    //
-    this.positions = { numComponents : 3 };
-    
-    // Initialize temporary arrays for the Cone's indices and vertex positions
-    //
-    var positions = [ 0.0, 0.0, 0.0 ];
-    var indices = [ 0 ];
-
-
-    // positions.push( 0.0, 0.0, 1.0 );
-    
-    // Close the triangle fan by repeating the first (non-center) point.
-    //
-
-    positions.push( 1.0, 0.0, 0.0 );
-    positions.push( 0.0, 1.0, 0.0 );
-
-    indices.push(1);
-    indices.push(2);
-
-    // Record the number of indices in one of our two disks that we're using to make the
-    //   cone.  At this point, the indices array contains the correct number of indices for a
-    //   single disk, and as we render the cone as two disks of the same size, this value is
-    //   precisely what we need.
-    //
-    this.indices = { count : indices.length };
-
-    // Now build up the list for the cone.  First, add the apex vertex onto the index list
-    //
-    indices.push(n + 1);
-
-    // Next, we need to append the rest of the vertices for the permieter of the disk.
-    // However, the cone's perimeter vertices need to be reversed since it's effectively a
-    // reflection of the bottom disk.
-    //
-    indices = indices.concat( indices.slice(1,n+2).reverse() );*/
-
     let program = initShaders(gl, "Bond-vertex-shader", "Bond-fragment-shader");
-   // let program = initShaders(gl, "Sphere-vertex-shader", "Sphere-fragment-shader");
-
-    const programInfo = {
-        program: program,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
-            textureCoord: gl.getAttribLocation(program, 'aTextureCoord'),
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
-            modelViewMatrix: gl.getUniformLocation(program, 'uModelViewMatrix'),
-            uSampler: gl.getUniformLocation(program, 'uSampler'),
-        }
-    };
 
     initTexture("https://webglfundamentals.org/webgl/resources/f-texture.png");
 
@@ -136,11 +61,6 @@ function Cube() {
         3, 7
     ];
 
-    /*var texcoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-    // Set Texcoords.
-    setTexcoords(gl);*/
-
     gl.getUniformLocation(program, "R")
     positions.numComponents = 3;
 
@@ -162,8 +82,7 @@ function Cube() {
     gl.activeTexture(gl.TEXTURE0);
     gl.uniform1i( gl.getUniformLocation(program, 'aTextureCoord'), 0);
 
-    // MV = gl.getUniformLocation(program, "MV");
-    // this.MV = mat4();  // mat4() is in MV.js
+
     let P = gl.getUniformLocation(program, "P");
     if (P) { this.P = mat4(); }
 
@@ -186,11 +105,6 @@ function Cube() {
         if (MV) { gl.uniformMatrix4fv(MV, false, flatten(this.MV)); }
         if (color) { gl.uniform4fv(color, flatten(this.color)); }
 
-        // Render the wireframe version of the cube
-        /**gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, edges.buffer );
-        gl.drawElements( gl.LINES, edges.length, gl.UNSIGNED_SHORT, 0 );*/
-
-        // Render the solid version of the cube
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indices.buffer );
         gl.drawElements( gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
     }
